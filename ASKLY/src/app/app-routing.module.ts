8060169d53f6d7fbf75f5ea801f682a90687723b
Thando <thandwem045@gmail.com>
2025-10-28 15:10:23 +0200

    Tested routing

--- ASKLY/src/app/app-routing.module.ts
+++ ASKLY/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultsComponent } from './results/results.component';
+import { HeaderComponent } from './header-component/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HeaderComponent },
+  { path: 'results', component: ResultsComponent }
+  // {},
+  // {},
+  // {},
+  // {},
+  // {},
+  // {},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
