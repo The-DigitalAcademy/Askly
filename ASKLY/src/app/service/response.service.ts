e5c03c84f80ab851ee6d2b8299a4154019a8cb1a
V-Ndaba <vusindaba11@gmail.com>
2025-10-28 12:07:14 +0200

    response to survey service

---
+++ ASKLY/src/app/service/response.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { SurveyService } from './survey.service';
+import { Observable, switchMap, throwError } from 'rxjs';
+import { response } from '../models/response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponseService {
+  private readonly url = '/api/response'
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly auth: AuthService,
+    private readonly surveyService: SurveyService
+  ) { }
+
+  submitSurvey(surveyID: number, answers: any[]): Observable<response>{
+    return this.surveyService.getAll().pipe(
+      //find the survey first
+      //@ts-ignore
+      switchMap(allSurveys => {
+        const currentSurvey = allSurveys.find(s => s.id === surveyID);
+        if(!currentSurvey?.isOpen)
+          return throwError(() => new Error('Survey not available'));
+
+        //do we have same number of answers?
+      if(answers.length !== currentSurvey.questions.length)
+        return throwError(() => new Error('Not all questions answered'));
+
+      for(const ans of answers){
+        const q = currentSurvey.questions.find(q => q.id === ans.questionId);
+        if(!q?.choices.some(c => c.id === ans.choiceId))
+          return throwError(() => new Error('Invalid Error'));
+      }
+      })
+    )
+  }
+}
