8746f4d733b870efab57dde948706f3ca98b4d03
Hloni4 <hlonikhoza98@gmail.com>
2025-10-29 13:10:42 +0200

    new branch changes

--- ASKLY/src/app/create-survey-compound/create-survey-compound.component.ts
+++ ASKLY/src/app/create-survey-compound/create-survey-compound.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router} from '@angular/router';
+import { throwError } from 'rxjs';
+import { user} from 'src/app/models/user';
+import { SurveyService} from ''
 
 @Component({
   selector: 'app-create-survey-compound',
