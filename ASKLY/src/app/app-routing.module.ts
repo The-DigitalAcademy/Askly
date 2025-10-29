de8ca687f0361533e38c12c48a671e09701d329b
NkeleMosia <mosiankele39@gmail.com>
2025-10-29 08:11:27 +0200

    Added routing to the view buttons

--- ASKLY/src/app/app-routing.module.ts
+++ ASKLY/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResultsComponent } from './results/results.component';
 import { HeaderComponent } from './header-component/header/header.component';
+import { SurveyDetailsComponent } from './survey-details/survey-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HeaderComponent },
-  { path: 'results', component: ResultsComponent }
-  // {},
+  { path: 'results', component: ResultsComponent },
+  { path: 'details', component: SurveyDetailsComponent},
   // {},
   // {},
   // {},
