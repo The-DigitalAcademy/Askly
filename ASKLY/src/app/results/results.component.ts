c49fc8f5237d924b61d5365266e543fb61b86afe
NkeleMosia <mosiankele39@gmail.com>
2025-10-29 14:14:41 +0200

    Updated html and css

--- ASKLY/src/app/results/results.component.ts
+++ ASKLY/src/app/results/results.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { SurveyService } from '../service/survey.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
+  standalone: false
 })
 export class ResultsComponent {
+  constructor(private surveyService: SurveyService) {}
+    survey = this.surveyService.getAll()
+    data: any[] = [];
 
+    ngOnInit() {
+      this.survey.subscribe((data) =>
+        this.data = data
+    )
+    }
 }
