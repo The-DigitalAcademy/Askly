79ef3ea16b4bd2ed52390f6c77737b6b224b1470
V-Ndaba <vusindaba11@gmail.com>
2025-10-29 14:19:48 +0200

    survey details logic updated

--- ASKLY/src/app/app-routing.module.ts
+++ ASKLY/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   { path: 'create-survey', component: CreateSurveyCompoundComponent },
   { path: 'register', component: RegisterPageComponent},
   { path: 'login', component: FormLogin},
-  { path: 'survey-detail:id', component: SurveyDetailsComponent},
+  { path: 'survey-detail/:id', component: SurveyDetailsComponent},
   // {},
   // {},
   // {},
--- ASKLY/src/app/survey-details/survey-details.component.ts
+++ ASKLY/src/app/survey-details/survey-details.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SurveyService } from '../service/survey.service';
 import { Observable, throwError } from 'rxjs';
 import { ResultsService } from '../service/results.service';
 import { surveyResults } from '../models/surveyResults';
 import { results } from '../models/result';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'survey-details',
   templateUrl: './survey-details.component.html',
   styleUrls: ['./survey-details.component.css']
 })
-export class SurveyDetailsComponent {
+export class SurveyDetailsComponent implements OnInit {
 
-  constructor(private resultsService: ResultsService){}
+  constructor(
+    private resultsService: ResultsService,
+    private readonly route: ActivatedRoute
+  ){}
   results: results[] = [];
+
+  ngOnInit(): void {
+    const surveyIdParam = this.route.snapshot.paramMap.get('id');
+    const survey_id = surveyIdParam ? Number(surveyIdParam) : null;
+    if (survey_id !== null && !Number.isNaN(survey_id)) {
+      this.getSurveyDetails(survey_id);
+    } else {
+      console.error('Invalid survey id');
+    }
+  }
 
   getSurveyDetails(id: number) {
     this.resultsService.getResults(id).subscribe({
