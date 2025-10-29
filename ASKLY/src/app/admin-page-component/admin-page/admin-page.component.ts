2df04bb6147f2a314d52b4889441cf231ee01fff
Thando <thandwem045@gmail.com>
2025-10-29 12:09:09 +0200

    Adde basic delete logic

--- ASKLY/src/app/admin-page-component/admin-page/admin-page.component.ts
+++ ASKLY/src/app/admin-page-component/admin-page/admin-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SurveyService } from 'src/app/service/survey.service';
 
+
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
@@ -9,24 +10,27 @@ import { SurveyService } from 'src/app/service/survey.service';
 })
 export class AdminPageComponent {
      constructor(private surveyService: SurveyService) {}
-     survey = this.surveyService.getAll()
+     survey = this.surveyService
      data: any[] = [];
    
      ngOnInit() {
-      this.survey.subscribe((data) =>
+      this.survey.getAll().subscribe((data) =>
           this.data = data
        )
      }
      post() {
-        console.log("My post is working")
+       
     }
 
-    delete(id: number){ {
-      console.log(this.data.id)
-      this.surveyService.delete(id)
+    deleteItem(id: number, index: number){ {
+      console.log(this.data.splice(index, 1))
     }}
 
     edit(){
       console.log("My edit is working")
+    }
+
+    isAdmin(){
+      console.log("Admin verified")
     }
 }
