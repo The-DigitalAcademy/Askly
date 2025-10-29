62479ff26b7ce69d09993f7595c42be956df3d63
NkeleMosia <mosiankele39@gmail.com>
2025-10-29 09:05:47 +0200

    Added routing on the admin button

--- ASKLY/src/app/app-routing.module.ts
+++ ASKLY/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ResultsComponent } from './results/results.component';
 import { HeaderComponent } from './header-component/header/header.component';
 import { SurveyDetailsComponent } from './survey-details/survey-details.component';
+import { FormLogin } from './login-page/component/form-login/form-login';
+import { AdminPageComponent } from './admin-page-component/admin-page/admin-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HeaderComponent },
   { path: 'results', component: ResultsComponent },
   { path: 'details', component: SurveyDetailsComponent},
-  // {},
+  { path: 'admin', component: AdminPageComponent},
   // {},
   // {},
   // {},
