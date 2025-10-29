8e0ddeee5e28356bd456872b9dafd8223908de6b
Thando <thandwem045@gmail.com>
2025-10-29 09:03:01 +0200

    Added Routing to pages

--- ASKLY/src/app/app-routing.module.ts
+++ ASKLY/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResultsComponent } from './results/results.component';
 import { HeaderComponent } from './header-component/header/header.component';
+import { AdminPageComponent } from './admin-page-component/admin-page/admin-page.component';
+import { CreateSurveyCompoundComponent } from './create-survey-compound/create-survey-compound.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HeaderComponent },
-  { path: 'results', component: ResultsComponent }
+  { path: 'results', component: ResultsComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'create-survey', component: CreateSurveyCompoundComponent },
   // {},
   // {},
   // {},
