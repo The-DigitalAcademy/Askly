0fc466b4e03c7a7b1c2cf0caef563d465ce9a9fc
V-Ndaba <vusindaba11@gmail.com>
2025-10-30 12:30:37 +0200

    added logout

--- ASKLY/src/app/header-component/header/header.component.ts
+++ ASKLY/src/app/header-component/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { user } from 'src/app/models/user';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
   standalone: false
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit{
+  currentUser: user | null = null;
+  isCoordinator: boolean = false;
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
+    this.isCoordinator = this.currentUser?.role === 'coordinator';
+  }
+  logout(): void {
+    this.authService.logout();
+    this.currentUser = null;
+    this.isCoordinator = false;
+    this.router.navigate(['/login']);
+  }
 }
